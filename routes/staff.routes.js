642dc07e3558c3af0312d668e0d979a79a908f8f
SnehalButani <snehalbutani03@gmail.com>
2024-01-20 12:37:12 +0530

    minro changes

--- routes/staff.routes.js
+++ routes/staff.routes.js
@@ -12,17 +12,16 @@ const storage = multer.diskStorage({
         const uploadPath = 'uploads/images';
         fs.mkdirSync(uploadPath, { recursive: true });
         cb(null, uploadPath);
-        console.log("ok")
     },
-    filename: function (req, files, cb) {
-        const fileExtension = path.extname(files.originalname);
+    filename: function (req, file, cb) {
+        const fileExtension = path.extname(file.originalname);
         const uniqueSuffix = Date.now() + Math.round(Math.random() * 1E9);
         cb(null, uniqueSuffix + fileExtension);
     },
 });
 const upload = multer({ storage: storage });
 
-router.post('/addstaff', upload.fields([{name:'image',maxCount:1}]), addStaff);
+router.post('/addstaff', upload.single('image'), addStaff);
 router.post('/editstaff', upload.single('image'), editStaff);
 router.post('/stafflist', staffList);
 router.post('/staffonedata', staffOneData);
